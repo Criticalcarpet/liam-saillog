1509751cadc72421efff42471f449b2530b4c0a8
Manik2375 <manikkpr2375@gmail.com>
2022-06-03 20:04:26 +0530

    feat(sidebar): create sidebar 80%

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import '../css/app.css'
+
+const COMPACT_WIDTH = 4;
+const SIDEBAR = document.querySelector('.sidebar'),
+    SIDEBAR_TOGGLE_BTN = document.querySelector(".sidebar__toggle-btn"),
+    SIDEBAR_LIST_ITEMS = document.querySelectorAll(".sidebar-list__item");
+
+SIDEBAR_TOGGLE_BTN.addEventListener("click", () => {
+    SIDEBAR.classList.toggle("sidebar_mode_compact");
+
+    if (SIDEBAR.classList.contains("sidebar_mode_compact")) {
+        SIDEBAR_TOGGLE_BTN.textContent = ">|";
+        SIDEBAR_LIST_ITEMS.forEach(item => item.classList.add("sidebar-list__item_mode_compact"))
+    } else {
+        SIDEBAR_TOGGLE_BTN.textContent = "|<";
+        SIDEBAR_LIST_ITEMS.forEach(item => item.classList.remove("sidebar-list__item_mode_compact"))
+    }
+})
