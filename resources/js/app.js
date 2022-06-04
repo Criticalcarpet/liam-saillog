aa45819730ab8683b36d96295b864593c6f952f3
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-06-04 12:29:15 +0530

    Initial setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import '../css/app.css'
+
+let getSidebar = document.querySelector('nav')
+let getToggle = document.getElementsByClassName('toggle')
+
+if (getToggle.length > 0) {
+  for (let i = 0; i < getToggle.length; i++) {
+    getToggle[i].addEventListener('click', function () {
+      getSidebar.classList.toggle('active')
+    })
+  }
+}
