54298e8d32ea1c728de224419b9c0d30a661b2f4
Manik2375 <manikkpr2375@gmail.com>
2022-06-05 16:44:50 +0530

    feat(sidebar): create sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 import '../css/app.css'
 
-const COMPACT_WIDTH = 4;
 const SIDEBAR = document.querySelector('.sidebar'),
     SIDEBAR_TOGGLE_BTN = document.querySelector(".sidebar__toggle-btn"),
-    SIDEBAR_LIST_ITEMS = document.querySelectorAll(".sidebar-list__item");
+    SIDEBAR_LIST_ITEMS = document.querySelectorAll(":not(.dropdown-container)> .sidebar-list > .sidebar-list__item"),
+    SIDEBAR_DROPDOWNS_INITIATORS = document.querySelectorAll(".sidebar-list__item > .sidebar__page-link[data-dropdown]")
 
 SIDEBAR_TOGGLE_BTN.addEventListener("click", () => {
     SIDEBAR.classList.toggle("sidebar_mode_compact");
-
     if (SIDEBAR.classList.contains("sidebar_mode_compact")) {
         SIDEBAR_TOGGLE_BTN.textContent = ">|";
         SIDEBAR_LIST_ITEMS.forEach(item => item.classList.add("sidebar-list__item_mode_compact"))
@@ -15,4 +14,16 @@ SIDEBAR_TOGGLE_BTN.addEventListener("click", () => {
         SIDEBAR_TOGGLE_BTN.textContent = "|<";
         SIDEBAR_LIST_ITEMS.forEach(item => item.classList.remove("sidebar-list__item_mode_compact"))
     }
+})
+
+SIDEBAR_DROPDOWNS_INITIATORS.forEach(initiator => {
+    initiator.addEventListener("click", (e) => {
+        if (SIDEBAR.classList.contains("sidebar_mode_compact")) return;
+        const openedDropdown = document.querySelector("[data-dropdown='open']");
+        if (openedDropdown)  openedDropdown.dataset.dropdown = "close";
+
+        if (e.currentTarget != openedDropdown) {
+            e.currentTarget.dataset.dropdown == "open" ?e.currentTarget.dataset.dropdown = "close" : e.currentTarget.dataset.dropdown = "open"
+        }
+    })
 })
